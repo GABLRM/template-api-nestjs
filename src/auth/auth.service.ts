67cead24da3dabd20bc452503df9a284226a3414
Gabriel Laroumanie <g.laroumanie@gmail.com>
2024-10-01 18:26:39 +0200

    :sparkles: feat: bearer token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-  constructor(private readonly usersService: UsersService) { }
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) { }
 
   async signIn(username: string, pass: string): Promise<any> {
     const user = await this.usersService.getOneByUsername(username);
-    console.log('user = ' + JSON.stringify(user))
-    console.log('user.password = ' + user.password)
     if (!user || !(await bcrypt.compare(pass, user.password))) {
       throw new UnauthorizedException("email or password incorrect");
     }
-    const { password, ...result } = user;
-    return result;
+    const playload = { sub: user.id, email: user.email };
+    return {
+      access_token: await this.jwtService.signAsync(playload),
+    };
   }
 }
