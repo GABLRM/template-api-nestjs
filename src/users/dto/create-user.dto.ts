114c655b98e214151546eb145168265990e28a17
Gabriel Laroumanie <g.laroumanie@gmail.com>
2024-10-01 21:54:52 +0200

    :sparkles: feat: user swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,17 +1,21 @@
 import { Contains, IsEmail, IsNotEmpty, IsString, IsStrongPassword, MinLength } from "class-validator";
 import { Role } from "../entities/user.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto {
 
+    @ApiProperty({description:"User username", example : "UserExample"})
     @IsNotEmpty()
     @MinLength(3)
     username!: string;
 
+    @ApiProperty({description: "User email", example: "user.example@mail.com"})
     @IsEmail()
     @IsNotEmpty()
     @IsString()
     email!: string;
 
+    @ApiProperty({description: "User password", example: "Password1234$"})
     @IsString()
     @MinLength(8)
     @IsNotEmpty()
