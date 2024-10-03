2a7141eb4ae097e243223a68d2955946335a1cf8
Gabriel Laroumanie <g.laroumanie@gmail.com>
2024-10-03 19:43:21 +0200

    :sparkles: feat: authentication swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDTO } from './dto/signin.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('Authentication')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Post('login')
+  @ApiOperation({ summary: "Login to account" })
+  @ApiResponse({ status: 201, description: 'User is connected' })
+  @ApiResponse({ status: 401, description: 'Email or password is incorrect' })
+  @ApiBody({ type: SignInDTO, description: "Data to login" })
   signIn(@Body() signInDto: SignInDTO) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
